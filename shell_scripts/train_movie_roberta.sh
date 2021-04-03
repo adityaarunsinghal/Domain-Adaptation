bcbb4b4179b37bdf8adf954fb492f827cfa2ce4f
Aditya Singhal <adityaarunsinghal@gmail.com>
2021-04-03 15:50:49 -0400

    update train script

--- shell_scripts/train_movie_roberta.sh
+++ shell_scripts/train_movie_roberta.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=128GB
 #SBATCH --time=3-00:00:00
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:2
 #SBATCH --mail-type=ALL
 #SBATCH -c 8
 #SBATCH --mail-user=adis@nyu.edu
@@ -22,21 +22,43 @@ conda activate
 
 # this was mlm continued on roberta
 
-export TRAIN_FILE=/scratch/as11919/Domain-Adaptation/data/simple_movie_text_MLM/ALL_DATA_train.txt
-export TEST_FILE=/scratch/as11919/Domain-Adaptation/data/simple_movie_text_MLM/ALL_DATA_test.txt
+export TRAIN_FILE=$SCRATCH/Domain-Adaptation/data/simple_movie_text_MLM/ALL_DATA_train.txt
+export TEST_FILE=$SCRATCH/Domain-Adaptation/data/simple_movie_text_MLM/ALL_DATA_test.txt
 
 python make_movies_dataset.py
 
-python /scratch/as11919/transformers/examples/language-modeling/run_mlm.py \
+python run_mlm_HF.py \
     --model_name_or_path roberta-base \
     --train_file $TRAIN_FILE \
     --validation_file $TEST_FILE \
     --do_train \
     --do_eval \
-    --output_dir /scratch/as11919/Domain-Adaptation/movie_roberta/roberta_DAPT_movies_model_withEVAL
+    --output_dir $SCRATCH/Domain-Adaptation/movie_roberta/roberta_DAPT_movies_model_withEVAL
 
 echo "Done!"
 "
+
+# default_metrics =   "attention_probs_dropout_prob": 0.1,
+#   "bos_token_id": 0,
+#   "eos_token_id": 2,
+#   "gradient_checkpointing": false,
+#   "hidden_act": "gelu",
+#   "hidden_dropout_prob": 0.1,
+#   "hidden_size": 768,
+#   "initializer_range": 0.02,
+#   "intermediate_size": 3072,
+#   "layer_norm_eps": 1e-05,
+#   "max_position_embeddings": 514,
+#   "model_type": "roberta",
+#   "num_attention_heads": 12,
+#   "num_hidden_layers": 12,
+#   "pad_token_id": 1,
+#   "position_embedding_type": "absolute",
+#   "transformers_version": "4.4.0.dev0",
+#   "type_vocab_size": 1,
+#   "use_cache": true,
+#   "vocab_size": 50265
+
 
 # this was movie_roberta from scratch
 # python DAPT_movies_scratch.py
